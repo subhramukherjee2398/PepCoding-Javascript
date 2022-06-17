9f0fbfac5497ff1697433937a1e9ef784ce75d89
Subhra Mukherjee <subhramukherjee560@gmail.com>
2022-06-17 22:36:44 +0530

    async await interview qns

---
+++ Events_Loop/Async & await Interview qns/int.js
@@ -0,0 +1,16 @@
+
+async function inc(x){
+    x  = x + await + 1;
+    return x;
+}
+
+async function increment(x){
+  x  = x + 1 
+  return x
+}
+
+inc(1).then(function(x){
+    increment(x).then(function(x){
+        console.log(x)
+    })
+})
