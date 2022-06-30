3a925ffc9281dd29ebd261b9f9d76cecff024300
Subhra Mukherjee <subhramukherjee560@gmail.com>
2022-06-30 20:19:13 +0530

    Pure FUnction

---
+++ Pure_Function.js
@@ -0,0 +1,17 @@
+// Example of impur function
+
+let a = 10
+
+const Impurefunc = (b) =>{
+    console.log("sum=> ",a+b)
+}
+
+Impurefunc(4);
+
+//Pure function
+
+const PureFunc = (a,b) =>{
+    return a+b
+}
+
+console.log(4,5)
